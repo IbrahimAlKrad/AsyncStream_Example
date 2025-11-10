5e1eb43f0cd18aad07f0da321f4a5c42b59159eb
IbrahimAlKrad <i.alkrad@hotmail.com>
2025-11-10 16:59:58 +0100

    init

---
+++ StreamingEx/StreamingExApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamingExApp.swift
+//  StreamingEx
+//
+//  Created by Ibrahim Al Krad on 10.11.25.
+//
+
+import SwiftUI
+
+@main
+struct StreamingExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
